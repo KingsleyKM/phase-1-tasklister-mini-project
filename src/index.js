d4c427309879e77f52a6607aa421528b02a1f6a2
Kingsley Maina <mainakingsley7@gmail.com>
2023-09-28 21:40:28 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ let form = document.querySelector("form")
+ form.addEventListener("submit",(e) => {
+    e.preventDefault()
+    handleTasks(document.querySelector("input").value)
+    form.reset()
+  })
 });
+function handleTasks (task){
+  console.log(task)
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.addEventListener("click",handleDelete)
+  btn.textContent = "x"
+  p.textContent = `${task} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector("#tasks").appendChild(p)
+  // document.querySelector("list").appendChild(p)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
